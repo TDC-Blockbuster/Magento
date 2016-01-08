b6a74665206f65842f48c63a44d1419b64102f40
Marc Eiberg Petersen <marc@eiberg.me>
2016-01-08 14:06:56 +0100

    Added danish lang.

--- Index.php
+++ Index.php
@@ -1,11 +1,18 @@
 <?php
 
 
-	public function sayHelloTo($name) {
-		$string = 'Hello ' . $name;
+	public function sayHelloTo($lang, $name) {
+
+		if ($lang == 'da-DK') {
+			$hello = 'Hej';
+		} else if ($lang == 'en-EN') {
+			$hello = 'Hello';
+		}
+		$string = $hello . ' ' . $name;
+
 		return $string;
 	}
 
-	echo sayHelloTo('Sanni! :)');
+	echo sayHelloTo('da-DK', 'Sanni! :)');
 	
 ?>
